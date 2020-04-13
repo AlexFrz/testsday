e475b528cc226a31b5154c6a44be98cc7f203de0
AlexFrz <62942629+AlexFrz@users.noreply.github.com>
2020-04-13 17:21:03 +0200

    Add files via upload

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,7 +1,4 @@
 def translate(word)
-	if word[0] == 'aeiou'
-		word.split.map { |word| word.chars.join }.join(' ')
-	else
-		word.split.map { |word| word.chars.rotate.join }.join(' ')	#start with consonnant
-	end
+ prefix = word[0, %w(a e i o u).map{|vowel| "#{word}aeiou".index(vowel)}.min]
+  "#{word[prefix.length..-1]}#{prefix}ay"
 end
