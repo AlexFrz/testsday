96b05b47cdae141a04600004bc0a6921d65e24fd
AlexFrz <62942629+AlexFrz@users.noreply.github.com>
2020-04-13 16:56:00 +0200

    Delete 06_pig_latin.rb

--- 06_pig_latin.rb
+++
@@ -1,7 +0,0 @@
-def translate(word)
-	if word[0] == 'aeiou'
-		word.split.map { |word| word.chars.join }.join(' ')
-	else
-		word.split.map { |word| word.chars.rotate.join }.join(' ')	#start with consonnant
-	end
-end
